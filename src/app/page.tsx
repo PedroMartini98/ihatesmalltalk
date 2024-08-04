cf1bfa8422bf507af35072f8e7a0f578acf3e624
Pedro H. Martini <Pedroh.martini@yahoo.com.br>
2024-08-04 08:11:21 -0300

    Tentando fazer o header caber em celular

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
   return (
     <main className="backgroundiv1 bg-cover min-h-screen overflow-hidden">
       <div className="flex  flex-col items-center justify-between ">
-        <div className="caixa mb-[6rem] w-[650px]">
+        <div className="caixa mb-[6rem] w-[650px] sm::w-[600px]">
           <p
             className={`text-metal-gold ${gv.className} px-5 text-center pt-5 overflow-hidden text-6xl hover:cursor-pointer`}
           >
@@ -34,6 +34,16 @@ export default function Home() {
                 A ideia é produzir perguntas interessantes sobre assuntos profundos sem a pretensão de sondar uma parte específica da vida de alguém. 
                 O próprio host tem uma iniciativa semelhante chamada "The Conversation Cards", vendida por 25 euros em seu site.
                  Esta é uma versão traduzida e gratuita feita com perguntas de brasileiros para brasileiros.
+            `}
+          />
+          <Accordion
+            title={"Como jogar:"}
+            description={`Junte amigos com os quais você quer conversar sobre a vida, o universo e tudo mais. 
+              Quando vocês apertarem "Jogar", verão várias perguntas reflexivas de pessoas anônimas ou não.
+               Embaixo da pergunta, haverá a resposta do autor para a própria pergunta, fornecendo um direcionamento 
+               sobre o tipo de resposta esperada. Então, cada um que tiver uma resposta interessante se manifesta e, 
+               no mundo ideal, vocês vão conversando sobre o assunto até ele se esgotar. Quando o assunto morrer, 
+              vocês passam para a próxima pergunta e repetem o processo até onde o coração mandar.
             `}
           />
           <Accordion
